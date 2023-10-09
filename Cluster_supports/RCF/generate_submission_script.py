2ac26e722f81bf79547b12150e95f5c21a548e5d
maxstw23 <74139226+maxstw23@users.noreply.github.com>
2023-10-09 02:17:15 -0700

    Update generate_submission_script.py

--- Cluster_supports/RCF/generate_submission_script.py
+++ Cluster_supports/RCF/generate_submission_script.py
@@ -25,15 +25,15 @@ def write_submission_script(para_dict_):
     <command>""")
                  
     if para_dict_["bayesFlag"]:
-        script.write("""./run_singularity.sh {0} {1} {2} {3} {4} {5} </command>
-""".format(para_dict_["paraFile"], random_seed, para_dict_["n_events_per_job"],
+        script.write("""singularity exec -B /direct -B /star -B /afs -B /gpfs {0} ./run_singularity.sh {1} {2} {3} {4} {5} {6} </command>
+""".format(para_dict["image_iwth_path"], para_dict_["paraFile"], random_seed, para_dict_["n_events_per_job"],
            para_dict_["n_threads"], random_seed, para_dict_["bayesFile"]))
     else:
-        script.write("""./run_singularity.sh {0} {1} {2} {3} {4} </command>
-""".format(para_dict_["paraFile"], random_seed, para_dict_["n_events_per_job"],
+        script.write("""singularity exec -B /direct -B /star -B /afs -B /gpfs {0} ./run_singularity.sh {1} {2} {3} {4} {5} </command>
+""".format(para_dict["image_iwth_path"], para_dict_["paraFile"], random_seed, para_dict_["n_events_per_job"],
            para_dict_["n_threads"], random_seed))
-    script.write("""
-    <shell>/bin/sh -c 'exec singularity exec -B /direct -B /star -B /afs -B /gpfs {0}</shell>""".format(para_dict_["image_with_path"]))
+    # script.write("""
+    # <shell>/bin/sh -c 'exec singularity exec -B /direct -B /star -B /afs -B /gpfs {0}</shell>""".format(para_dict_["image_with_path"]))
     
     script.write("""    
     <ResourceUsage>
